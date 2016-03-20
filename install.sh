1e8fee125240b63ca507493d1384376a8a3d65b9
Chad Elliott <THEtheChad@users.noreply.github.com>
2016-03-20 11:28:54 -0500

    altered to prompt for each installation

--- install.sh
+++ install.sh
@@ -10,72 +10,58 @@ function waypoint() {
   fi
 
   echo;
-  echo -e "\033[1;34;47m  $1 installation has completed!  \033[m"
-  echo "Would you like to continue the installation process? (y/n)"
+  echo "Would you like to install $1? (y/n)"
   read -p "$ " -n 1
   echo;
 
   if [[ $REPLY =~ ^[Nn]$ ]]; then
-    exit 1
+    return 1
   fi
 }
 
 
-
-# https://github.com/rupa/z
-# z, oh how i love you
-mkdir -p code/z
-curl https://raw.github.com/rupa/z/master/z.sh > code/z/z.sh
-chmod +x code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-waypoint "Z"
-
-
-
-# homebrew!
-echo "Make sure the CLI tools for xCode are installed."
-echo "Are you ready to continue? (y/n)"
-read -p "$ " -n 1
-echo
-
-if [[ $REPLY =~ ^[Nn]$ ]]; then
-  exit 1
+if waypoint "Z"; then
+  # https://github.com/rupa/z
+  # z, oh how i love you
+  mkdir -p code/z
+  curl https://raw.github.com/rupa/z/master/z.sh > code/z/z.sh
+  chmod +x code/z/z.sh
+  # also consider moving over your current .z file if possible. it's painful to rebuild :)
 fi
 
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" && brew doctor
 
-waypoint "Homebrew"
-
-
-
-# https://rvm.io
-# rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
-
-waypoint "RVM"
-
-
-
-# https://github.com/creationix/nvm
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-waypoint "NVM"
+if waypoint "Homebrew"; then
+  # homebrew!
+  echo "Make sure the CLI tools for xCode are installed."
+  echo "Are you ready to continue? (y/n)"
+  read -p "$ " -n 1
+  echo
+  
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    exit 1
+  fi
+  
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" && brew doctor
+fi
 
 
+if waypoint "RVM"; then
+  # https://rvm.io
+  # rvm for the rubiess
+  curl -L https://get.rvm.io | bash -s stable --ruby
+fi
+
+
+if waypoint "NVM"; then
+  # https://github.com/creationix/nvm
+  curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+fi
 
 # jq
-brew install jq
-
-waypoint "jq"
-
-
+# brew install jq
 
 # ngrep
-brew install ngrep
-
-waypoint "ngrep"
-
+# brew install ngrep
 
 
 # Update mac config
